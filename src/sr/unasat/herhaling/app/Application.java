99178f91f69a326cf0dea8606d70df39f1ac84e2
Maarten Narain <mnarain@qualogy.com>
2020-11-07 09:53:33 -0300

    Initial commit

---
+++ src/sr/unasat/herhaling/app/Application.java
@@ -0,0 +1,11 @@
+package sr.unasat.herhaling.app;
+
+import sr.unasat.herhaling.services.TafelService;
+
+public class Application {
+    public static void main(String[] args) {
+        TafelService ts = new TafelService();
+        ts.tafelVan10ForLoop();
+        ts.tafelVan10WhileLoop();
+    }
+}
---
+++ src/sr/unasat/herhaling/services/TafelService.java
@@ -0,0 +1,23 @@
+package sr.unasat.herhaling.services;
+
+public class TafelService {
+
+    public void tafelVan10ForLoop() {
+        System.out.println("Running tafelVan10ForLoop");
+        for (int multiplier = 1; multiplier <= 10; multiplier++) {
+            System.out.println(multiplier + " * " + 10 + " = " + (multiplier * 10));
+        }
+        System.out.println();
+    }
+
+    public void tafelVan10WhileLoop() {
+        System.out.println("Running tafelVan10WhileLoop");
+        int multiplier = 1;
+        while (multiplier <= 10) {
+            System.out.println(multiplier + " * " + 10 + " = " + (multiplier * 10));
+            multiplier++;
+        }
+        System.out.println();
+    }
+
+}
