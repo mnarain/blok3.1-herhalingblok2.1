f006d4c0481e47cb27569681787c14d2a3974877
Maarten Narain <mnarain@qualogy.com>
2020-11-07 10:31:13 -0300

    tafel van x opdracht 3 parameters

--- src/sr/unasat/herhaling/app/Application.java
+++ src/sr/unasat/herhaling/app/Application.java
@@ -5,7 +5,9 @@ import sr.unasat.herhaling.services.TafelService;
 public class Application {
     public static void main(String[] args) {
         TafelService ts = new TafelService();
-        ts.tafelVan10ForLoop();
-        ts.tafelVan10WhileLoop();
+        //ts.tafelVan10ForLoop();
+        //ts.tafelVan10WhileLoop();
+        ts.tafelVanXForLoop(55);
+        ts.tafelVanXWhileLoop(55);
     }
 }
--- src/sr/unasat/herhaling/services/TafelService.java
+++ src/sr/unasat/herhaling/services/TafelService.java
@@ -22,7 +22,26 @@ public class TafelService {
 
     //Implementeer de onderstaande 2 methods
     //tafelVanXForLoop
+    public void tafelVanXForLoop(int tafel) {
+        System.out.println("Running tafel van " + tafel + " ForLoop");
+        for (int multiplier = 1; multiplier <= 10; multiplier++) {
+            System.out.println(multiplier + " * " + tafel + " = " + (multiplier * tafel));
+        }
+        System.out.println();
+    }
     //tafelVanXWhileLoop
     // X is d tafel die je wenst uit te printen. gebruik een parameter
+    public void tafelVanXWhileLoop(int tafel) {
+        System.out.println("Running tafel van " + tafel + " WhileLoop");
+        int multiplier = 1;
+        while (multiplier <= 10) {
+            System.out.println(multiplier + " * " + tafel + " = " + (multiplier * tafel));
+            multiplier++;
+        }
+        System.out.println();
+    }
+    // implementeer onderstaande methods nu met 3 parameters en volgorde (begin,eind,tafel)
+    //tafelVanXForLoop
+    //tafelVanXWhileLoop
 
 }
